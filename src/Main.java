a158e75cde689e4ea651cd7725787f09d819ac1f
Natalya Bolgova <mnatala550@gmail.com>
2020-06-11 16:30:23 +0200

    Commitfirst

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentaccount = 2000000000;
+        int transfer = 500000000;
+        int total = currentaccount+transfer;
+        System.out.println(total);
+    }
+}
